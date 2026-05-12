13b6f5f0f43b361f3ddb84edc25516da0db9850f
Baek Jihoon <133375485+Chuseok22@users.noreply.github.com>
2026-05-13 01:01:33 +0900

    feat: project init

---
+++ src/main/java/com/chuseok22/umbrellareturn/UmbrellaReturnApplication.java
@@ -0,0 +1,13 @@
+package com.chuseok22.umbrellareturn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmbrellaReturnApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UmbrellaReturnApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/chuseok22/umbrellareturn/UmbrellaReturnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuseok22.umbrellareturn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmbrellaReturnApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
